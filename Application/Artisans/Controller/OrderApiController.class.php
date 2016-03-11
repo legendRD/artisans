840fae5cc75d800a39e866b28ecaa2d82588e0f8
legendRD <masterfengcnpc@163.com>
2016-03-11 16:14:02 +0800

    Update OrderApiController.class.php

--- Application/Artisans/Controller/OrderApiController.class.php
+++ Application/Artisans/Controller/OrderApiController.class.php
@@ -11,8 +11,47 @@ class OrderApiController extends CommonController {
     private $_payment_type	= array(100,200,300,400); //微信100，网银200，网页淘宝300，手机淘宝400 
     
     //订单接口
-    private $_getTrade_url	= "http://localhost/order/addor"; //生成订单号地址
-	  private $_orderPay_url	= "http://localhost/Paycenter/CreatePayInfo"; //支付接口
-	  private $_getStatus		= "http://localhost/order/list"; //获取订单状态地址
-	  private $_changeUrl = "http://localhost/Qrcode/shorturl"; //长连接变为短连接
+    private $_getTrade_url	= "http://localhost/order/addor"; 	      //生成订单号地址
+    private $_orderPay_url	= "http://localhost/Paycenter/CreatePayInfo"; //支付接口
+    private $_getStatus		= "http://localhost/order/list"; 	      //获取订单状态地址
+    private $_changeUrl 	= "http://localhost/Qrcode/shorturl"; 	      //长连接变为短连接
+    
+    //app微信支付
+    private $_appid		= '';
+    private $_appkey		= '';
+    private $_partnerkeyid	= '';
+    private $_partnerkey	= '';
+    
+    //日志
+    private $_update_order_status	= "/share/weixinLog/artisans/pay_order/update_order";	//订单日志
+    private $_pay_log			= '/share/pay_log_url/webArtisans/'; 			//vmall请求更新订单状态
+    
+    private $_card_type		= array(0=>'使用抵扣',1=>'使用卡券',2=>'使用红包');
+    private $_pay_way_type	= array(1=>'线下支付',2=>'微信系统微信支付',3=>'vmall微信支付',4=>'vmall支付宝支付');
+    private $_pay_way		= array(1,2,3,4);
+    private $_pay_process	= array(1,2,3,4);     //支付流程：1正常支付，2客服引导，3客服专家在线，4.距离大于40公里支付
+    private $_update_type	= array(100,200,300); //100取消订单
+    private $_craft_step	= array(1,2,3,4,5,6); //XXX服务流程
+    private $_scrypt_pwd	= '';
+    
+    public function _initialize() {
+	   $access_token	= I('request.access_token');
+	   $this->_access_token	= D("Token")->checkAccessToken($access_token);	//检测token
+	   //$this->_access_token = true;//测试
+    }
+    
+    //获取token
+    public function getCrtAuth() {
+    	$post_date   = I('request.');
+    	$source_from = $post_data['source_from'];
+    	if(empty($source_from)) {
+    		$this->returnJsonData(300);
+    	}
+    	$access_token = '';
+    	if($source_from == 'crt_api') {
+    		$access_token = D("Token")->apiAccessToken();
+    	}
+    	$data['access_token'] = $access_token;
+    	$this->returnJsonData(200, $data);
+    }
 }
