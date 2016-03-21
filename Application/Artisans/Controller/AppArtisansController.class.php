2f3466896d59fa1aedff1325793aa66b27232567
legendRD <masterfengcnpc@163.com>
2016-03-21 11:41:17 +0800

    Create AppArtisansController.class.php

---
+++ Application/Artisans/Controller/AppArtisansController.class.php
@@ -0,0 +1,7 @@
+<?php
+namespace Artisans\Controller;
+class AppArtisansController extends CommonController {
+      //头像地址
+      private $_headimg_upload_path = '/share/weixinImg/artisans/app/headImg/";
+      
+}
