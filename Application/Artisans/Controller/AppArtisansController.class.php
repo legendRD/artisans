5fd3d892cc1a1696c820d1e7a2183f78e8d26644
legendRD <masterfengcnpc@163.com>
2016-03-21 15:23:13 +0800

    Update AppArtisansController.class.php

--- Application/Artisans/Controller/AppArtisansController.class.php
+++ Application/Artisans/Controller/AppArtisansController.class.php
@@ -62,17 +62,96 @@ class AppArtisansController extends CommonController {
       
       //首页广告位
       public function getBanner() {
+            $postData = I('request.');
+            $this->wInfoLog('广告位，IP：'.get_ip());
+            $this->wInfoLog($postData, '接收参数=>');
             
+            $source = (int)$postData['type'];	//平台类型。1 微信，2 安卓， 3 IOS
+            if(empty($source)) {
+            	$this->returnJsonData(300);
+            }
+            
+            $comm_model = D('Comm');
+            $getbannerInfo = $comm_model->getbanner($source);
+            if($getbannerInfo && is_array($getbannerInfo)) {
+            	foreach($getbannerInfo as $val) {
+            		$hash['title']    = (string)$val['Title'];
+            		$hash['imgUrl']   = $val['ImgUrl'];
+            		$hash['clickUrl'] = (string)$val['Url'];
+            	}
+            	$this->returnJsonData(200, $hash);
+            }else{
+            	$this->returnJsonData(404);
+            }
       }
       
       //给用户发送验证码
       public function sendVerifyCode() {
-            
+            $postData	= I('request.');
+	    $this->wInfoLog('发送验证码,IP:'.get_ip());
+	    $this->wInfoLog($postData,'接收参数=>');
+	    
+	    $register_phone = trim($postData['phone']);
+	    if(empty($register_phone)) {
+	    	$this->returnJsonData(300);
+	    }
+	    $check_phone_status = check_phone($register_phone);
+	    if(!($check_phone_status)) {
+	    	$this->returnJsonData(1003);
+	    }
+	    
+	    $code = mt_rand(1000, 9999);
+	    $content = 'XXXXX验证码('.$code.')';
+	    $sendVerify = D('Comm')->sendShortMsg($register_phone, $content, 'artisans_register');
+	    if($sendVerify['SendMessageResult']['Resultcode'] == '00') {
+	    	$cdate = date('Y-m-d H:i:s');
+	    	$edate = date('Y-m-d H:i:s', time()+1800);
+	    	
+	    	$where = array('Phone'=>$register_phone, 'Source'=>0);
+	    	M('cut_captcha')->where($where)->delete();
+	    	
+	    	$data = array(
+	    		'Phone'=>$register_phone,
+	    		'Captcha'=>$code,
+	    		'CreaterTime'=>$cdate,
+	    		'LoseTime'=>$edate,
+	    		'Source'=>0
+	    	);
+	    	$id = M('cut_captcha')->add($data);
+	    	if($id) {
+	    		$this->returnJsonData(200);
+	    	}
+	    }
+	    $this->returnJsonData(500);
       }
       
       //查看验证码
       public function checkVerifyCode() {
-            
+            $postData = I('request.');
+            $this->wInfoLog('发送验证码,IP:'.get_ip());
+	    $this->wInfoLog($postData,'接收参数=>');
+	    
+	    $register_phone = trim($postData['phone']);
+	    $code	    = trim($postData['code']);
+	    if(!($register_phone && $code)) {
+	    	$this->returnJsonData(300);
+	    }
+	    $check_phone_status = check_phone($register_phone);
+	    if(!($check_phone_status)) {
+	    	$this->returnJsonData(1003);
+	    }
+	    
+	    $where = array('Phone'=>$register_phone, 'Captcha'=>$code, 'Source'=>0);
+	    $edate = M('cut_captcha')->where($where)->getField('LoseTime');
+	    if($edate) {
+	    	$second = time()-strtotime($edate);
+	    	if($second > 0) {
+	    		$this->returnJsonData(1004);
+	    	}else{
+	    		$this->returnJsonData(200);
+	    	}
+	    }
+	    $this->returnJsonData(500);
       }
       
       /**
@@ -84,7 +163,63 @@ class AppArtisansController extends CommonController {
 	 * @return	string
 	 */
 	 public function addUser() {
-	       
+	        $postData	= I('request.');
+		$this->wInfoLog('登陆接口,IP:'.get_ip());
+		$this->wInfoLog($postData,'接收参数=>');
+		
+		if(isset($postData['phone']) && isset($postData['code']) && isset($postData['login_time'])) {
+			if(!check_phone($postData['phone'])) {
+				$this->returnJsonData(1003);
+			}
+			
+			$where['Phone']   = $postData['phone'];
+			$where['Captcha'] = $postData['code'];
+			$where['Source']  = 0;
+			
+			$codeStatus 	  = M('cut_captcha')->where($where)->find();
+			
+			if($codeStatus && $codeStatus['LoseTime']<date('Y-m-d H:i:s')) {
+				$this->returnJsonData(1004);
+			}
+			if(!$codeStatus) {
+				$this->returnJsonData(508);
+			}
+			if($codeStatus) {
+				//用户注册
+				$base_info_s = array(
+					'source_from'=>'app',
+					'user_type'=>'other',
+					'type'=>200,
+					'username'=>$postData['phone']
+				);
+				
+				//注册用户中心
+				$user_center_info = send_curl(C('ArtisansApi').'/UserCenterApi/loginUser', $base_info_s);
+				$parse_data	  = json_decode($user_center_info, true);
+				
+				if($parse_data['code']!=200) {
+					$this->returnJsonData(500);
+				}
+				if(isset($postData['cid'])) {
+					$param = array(
+						'cid'=>$postData['cid'],
+						'uid'=>$parse_data['data']['uid'],
+						'login_time'=>$postData['login_time']
+					);
+					$isBind = $this->uiBindCid($param);
+					if($isBind['status']==200) {
+						$data['user_id'] = $isBind['data']['user_id'];
+						$data['cadate']  = $isBind['data']['cdate'];
+					}
+				}else{
+					$data['user_id'] = $parse_data['data']['uid'];
+					$data['cadate']  = $isBind['data']['cdate'];
+				}
+				$this->returnJsonData(200, $data);
+			}
+		}else{
+			$this->returnJsonData(300);
+		}
 	 }
 	 
 	 //uid绑定cid 接口
