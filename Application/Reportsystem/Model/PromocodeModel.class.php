53f857841bc0aede2b5cb855ea8bec6fdbe0d494
legendRD <masterfengcnpc@163.com>
2016-03-24 14:23:23 +0800

    Create PromocodeModel.class.php

---
+++ Application/Reportsystem/Model/PromocodeModel.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Reportsystem\Model;
+use Think\Model;
+class PromocodeModel extends Model {
+      
+      protected $autoCheckFields = false;
+      
+      //真实表
+      public function getTrueTable($shop_id) {
+             
+      }
+}
