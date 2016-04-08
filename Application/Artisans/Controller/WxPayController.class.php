59d8d2c8607be987a300fbfeded1ccbd84956eb9
legendRD <masterfengcnpc@163.com>
2016-04-08 10:12:29 +0800

    Update WxPayController.class.php

--- Application/Artisans/Controller/WxPayController.class.php
+++ Application/Artisans/Controller/WxPayController.class.php
@@ -1,5 +1,18 @@
 <?php
 namespace Artisans\Controller;
 class WxPayController extends CommonController {
-
+      private $_pay_log_url = '/share/pay_log_url/weixin/original';                       //订单支付日志
+      private $_error_url   = '/share/pay_log_url/weixin/original/error.log';             //订单失败日志
+      private $_success_url = '/share/pay_log_url/weixin/original/caifutong_success.log'; //财付通成功返回日志
+      private $_update_url  = '/share/pay_log_url/weixin/original/update_status.log';     //更新订单日志
+      private $_findwx_order_url = '/share/pay_log_url/weixin/original/findwx_order.lgo'; //重新生成订单
+      
+      public function notice2() {
+        
+      }
+      
+      //微信回
+      public function notice() {
+        
+      }
 }
