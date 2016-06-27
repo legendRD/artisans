931cdf3cf911b65cb4a4f965ecc901033bf05ae8
legendRD <masterfengcnpc@163.com>
2016-06-27 10:28:58 +0800

    Update TestController.class.php

--- Application/Artisans/Controller/TestController.class.php
+++ Application/Artisans/Controller/TestController.class.php
@@ -1,5 +1,15 @@
 <?php
 namespace Artisans\Controller;
 class TestController extends CommonController {
-
+      
+      public function jpush_push() {
+             $data['platform']    = $data['platform'] ? $data['platform'] : 'all';
+             $data['audience']    = '{"registration_id":["020ada8a498"]}';
+             $data['msg_content'] = '内容';
+             $data['title']       = '标题';
+             $data['content_type']= '类型';
+             $data['extras']      = array("type"=>"10", "order_id"=>"20");
+             $ret = D('Jpushaa')->jpushpush($data);
+             dump($ret);
+      }
 }
