8a577d1930e3474f3a3560c1cfce049015d8a90d
legendRD <masterfengcnpc@163.com>
2016-07-01 11:30:06 +0800

    Create JpushaaModel.class.php

---
+++ Application/Artisans/Model/JpushaaModel.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Artisans\Model;
+use Think\Model;
+use JPush\Model as M;
+use JPush\JPushClient;
+use JPush\Exception\APIConnectionException;
+use JPush\Exception\APIRequestException;
+
+class JpushaaModel extends Model {
+}
